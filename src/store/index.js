be79a02edeafaea552c68ccf65927e050bf5c302
Starkov Sergey <starkovsr@gmail.com>
2022-10-21 20:07:44 +0300

    feat: add redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,18 @@
 import { createStore } from 'redux'
 import { rootReducer } from './root-reducer'
+import  storage  from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['positions', 'filters'],
+  // blacklist: [],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = createStore(
-  rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+export const persistor = persistStore(store)
